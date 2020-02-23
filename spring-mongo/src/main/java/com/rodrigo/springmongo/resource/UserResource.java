5c59463d0246bbf651477d22b2e2e64399d13c91
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-02-22 23:21:24 -0300

    Criando UserDTO e obtendo um usuário por id.

--- spring-mongo/src/main/java/com/rodrigo/springmongo/resource/UserResource.java
+++ spring-mongo/src/main/java/com/rodrigo/springmongo/resource/UserResource.java
@@ -1,14 +1,17 @@
 package com.rodrigo.springmongo.resource;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.rodrigo.springmongo.domain.User;
+import com.rodrigo.springmongo.dto.UserDTO;
 import com.rodrigo.springmongo.service.UserService;
 
 @RestController
@@ -19,8 +22,18 @@ public class UserResource {
 	UserService service;
 
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
-		return ResponseEntity.ok().body(service.findAll());
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<User> list = service.findAll();
+		List<UserDTO> listUserDto = list.stream()
+				.map(u -> new UserDTO(u))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok().body(listUserDto);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User user = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 	
 }
--- spring-mongo/src/main/java/com/rodrigo/springmongo/service/UserService.java
+++ spring-mongo/src/main/java/com/rodrigo/springmongo/service/UserService.java
@@ -1,11 +1,13 @@
 package com.rodrigo.springmongo.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigo.springmongo.domain.User;
+import com.rodrigo.springmongo.exception.ObjectNotFoundException;
 import com.rodrigo.springmongo.repository.UserRepository;
 
 @Service
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll() {
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não localizado."));
 	}
 }
