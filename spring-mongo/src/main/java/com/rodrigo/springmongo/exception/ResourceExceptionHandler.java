58111c640facdebb8798214abce4a920ec6b0ee7
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-02-24 22:26:44 -0300

    Ajustando o método.

--- spring-mongo/src/main/java/com/rodrigo/springmongo/exception/ResourceExceptionHandler.java
+++ spring-mongo/src/main/java/com/rodrigo/springmongo/exception/ResourceExceptionHandler.java
@@ -13,11 +13,12 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ObjectNotFoundException.class)
 	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError standardError = new StandardError(System.currentTimeMillis(), 
-								  status.value(), 
-								  "Não encontrado", 
-								  e.getMessage(),
-				 				  request.getRequestURI());
+		StandardError standardError = new StandardError(
+											System.currentTimeMillis(), 
+											status.value(), 
+											"Não encontrado", 
+											e.getMessage(),
+											request.getRequestURI());
 		return ResponseEntity.status(status).body(standardError);
 	}
 }
