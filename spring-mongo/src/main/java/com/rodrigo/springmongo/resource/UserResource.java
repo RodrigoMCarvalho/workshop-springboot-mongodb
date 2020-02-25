1a0f8e5c9dbc0ca112ddc0b4af8ca9e5e7aebce9
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-02-24 22:16:54 -0300

    Criando os métodos POST, DELETE e PUT.

--- spring-mongo/src/main/java/com/rodrigo/springmongo/resource/UserResource.java
+++ spring-mongo/src/main/java/com/rodrigo/springmongo/resource/UserResource.java
@@ -1,21 +1,27 @@
 package com.rodrigo.springmongo.resource;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.rodrigo.springmongo.domain.User;
 import com.rodrigo.springmongo.dto.UserDTO;
 import com.rodrigo.springmongo.service.UserService;
 
 @RestController
-@RequestMapping(value= "/users")
+@RequestMapping("/users")
 public class UserResource {
 	
 	@Autowired
@@ -35,5 +41,47 @@ public class UserResource {
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(user));
 	}
+	
+	@PostMapping()
+	public ResponseEntity<?> insert(@RequestBody UserDTO userDTO) {
+		User user = service.fromDTO(userDTO);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder
+						.fromCurrentRequest()
+						.path("/{id}")
+						.buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable String id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<?> update(@RequestBody UserDTO userDTO, @PathVariable String id) {
+		User user = service.fromDTO(userDTO);
+		user.setId(id);
+		user = service.update(user);
+		return ResponseEntity.noContent().build();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 }
--- spring-mongo/src/main/java/com/rodrigo/springmongo/service/UserService.java
+++ spring-mongo/src/main/java/com/rodrigo/springmongo/service/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rodrigo.springmongo.domain.User;
+import com.rodrigo.springmongo.dto.UserDTO;
 import com.rodrigo.springmongo.exception.ObjectNotFoundException;
 import com.rodrigo.springmongo.repository.UserRepository;
 
@@ -24,4 +25,44 @@ public class UserService {
 		Optional<User> user = repo.findById(id);
 		return user.orElseThrow(() -> new ObjectNotFoundException("Usuário não localizado."));
 	}
+	
+	public User insert(User user) {
+		return repo.insert(user);
+	}
+	
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User user) {
+		User novoUser = findById(user.getId());
+		updateData(novoUser, user);
+		return repo.save(novoUser);
+	}
+	
+	private void updateData(User novoUser, User user) {
+		novoUser.setName(user.getName());
+		novoUser.setEmail(user.getEmail());
+	}
+
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
