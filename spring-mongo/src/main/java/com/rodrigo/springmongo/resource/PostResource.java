53fd75936de0ddf657593897785e4408c08dab83
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-02-26 21:57:44 -0300

    Consulta com vários critérios

--- spring-mongo/src/main/java/com/rodrigo/springmongo/resource/PostResource.java
+++ spring-mongo/src/main/java/com/rodrigo/springmongo/resource/PostResource.java
@@ -1,5 +1,6 @@
 package com.rodrigo.springmongo.resource;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,17 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping("/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(@RequestParam(value="text", defaultValue="") String text,
+												 @RequestParam(value="minDate", defaultValue="") String minDate,
+												 @RequestParam(value="maxDate", defaultValue="") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L)); //valor default 01/01/1970
+		Date max = URL.convertDate(maxDate, new Date());   //valor default é a data atual
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 	
--- spring-mongo/src/main/java/com/rodrigo/springmongo/service/PostService.java
+++ spring-mongo/src/main/java/com/rodrigo/springmongo/service/PostService.java
@@ -1,5 +1,6 @@
 package com.rodrigo.springmongo.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,6 +28,30 @@ public class PostService {
 	
 	public List<Post> findByTitle(String text) {
 		//return postRepository.findByTitleContainingIgnoreCase(text); usando query method
-		return postRepository.searchTitle(text);                      //usando @Query ( mesmo resultado )
+		return postRepository.searchTitle(text);                     //usando @Query ( mesmo resultado )
 	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000); //data + 24h	
+		return postRepository.fullSearch(text, minDate, maxDate);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
